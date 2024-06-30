fd93e1c9b09c8f69ed90f06d01d9c5869cb1f9b3
Nguyễn Đình Sơn <120614021+dihson103@users.noreply.github.com>
2024-06-30 20:41:20 +0700

    update init role db

--- src/WebApi/InitialData/DbInitializer.cs
+++ src/WebApi/InitialData/DbInitializer.cs
@@ -121,11 +121,11 @@ public class DbInitializer
     {
         var roles = new List<Role>
         {
-            Role.Create(new CreateRoleCommand("MAIN_ADMIN", "HuyVu's father")),
-            Role.Create(new CreateRoleCommand("BRAND_ADMIN", "HuyVu's father")),
-            Role.Create(new CreateRoleCommand("COUNTER", "HuyVu's father")),
-            Role.Create(new CreateRoleCommand("DRIVER", "HuyVu's father")),
-            Role.Create(new CreateRoleCommand("USER", "HuyVu's father"))
+            Role.Create(new CreateRoleCommand("MAIN_ADMIN", "Quản lý hệ thống")),
+            Role.Create(new CreateRoleCommand("BRAND_ADMIN", "Quản lý cơ sở")),
+            Role.Create(new CreateRoleCommand("COUNTER", "Quản lý số lượng")),
+            Role.Create(new CreateRoleCommand("DRIVER", "Nhân viên vận chuyển")),
+            Role.Create(new CreateRoleCommand("USER", "Nhân viên thường"))
         };
 
         context.Roles.AddRange(roles);
